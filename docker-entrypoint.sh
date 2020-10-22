5302994bddd30c6747666c1e2e0b7006960e8b13
Ronny Fischer <ronny.fischer2@dpdhl.com>
2020-10-22 18:11:56 +0200

    fix merge stuff

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-<<<<<<< HEAD
-/usr/bin/mosquitto_passwd -b /etc/mosquitto/conf.d/passwd ${MQTT_USER} ${MQTT_PASS}
-
-envsubst '${MQTT_USER}' < /etc/mosquitto/conf.d/acl.template > /etc/mosquitto/conf.d/acl
-=======
 /usr/bin/mosquitto_passwd -b /etc/mosquitto/conf.d/passwd "${MQTT_USER}" "${MQTT_PASS}"
 
 envsubst "${MQTT_USER}" < /etc/mosquitto/conf.d/acl.template > /etc/mosquitto/conf.d/acl
->>>>>>> 13695470cf115e9c69f5e36c1b9f400bef430363
 
 exec "$@"
